270c9358d4b5be862f6cc8443b5387c6c238fb80
Angelos Antoniades <anillusionist9050@gmail.com>
2024-01-17 04:18:27 +0200

    Fifth Commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,7 @@ function App() {
     let screenScale, screenPosition;
 
     if (window.innerWidth < 768) {
-      screenScale = [0.9, 0.9, 0.9];
+      screenScale = [0.7, 0.7, 0.7];
       screenPosition = [0, -6.5, -43.4];
     } else {
       screenScale = [0.7, 0.7, 0.7];
