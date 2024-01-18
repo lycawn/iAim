b3ad56bc51c261b14b62e8bcd871c11a93a896bd
lycawn <69188947+lycawn@users.noreply.github.com>
2024-01-18 04:57:51 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,7 +13,7 @@ function App() {
     let screenScale, screenPosition;
 
     if (window.innerWidth < 768) {
-      screenScale = [0.8, 0.8, 0.8];
+      screenScale = [0.7, 0.7, 0.7];
       screenPosition = [14, -6.5, -43.4];
     } else {
       screenScale = [0.9, 0.9, 0.9];
