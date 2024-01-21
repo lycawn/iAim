70c3292e94ffe69b117e54580d9d30827d0c6ea5
Angelos Antoniades <anillusionist9050@gmail.com>
2024-01-21 17:46:46 +0200

    12th commitment

--- src/components/Models/GhosKitty.jsx
+++ src/components/Models/GhosKitty.jsx
@@ -14,7 +14,7 @@ import { useEffect, useRef } from 'react';
 import { useGLTF } from '@react-three/drei';
 import { useFrame, useThree } from '@react-three/fiber';
 
-import islandScene from './assets/ghost_kitty.glb';
+import islandScene from '../assets/ghost_kitty.glb';
 
 export function GhostKitty({
   isRotating,
